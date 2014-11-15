66c1119cb5a754aa827a82a0abdbc08e521684b9
Efrain Ayllon <jesusayllonroa@gmail.com>
2014-11-15 13:11:22 -0600

    Before serializing

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,5 +1,7 @@
 class Api::PlacesController < ApplicationController
 
+protect_from_forgery with: :null_session
+
  def index
     @places = Place.all
     render json: @places
@@ -11,6 +13,10 @@ class Api::PlacesController < ApplicationController
     render json: @place
   end
 
-
+def update
+    @place = Place.find(params[:id])
+    @place.update(params.require(:place).permit(:name, :city))
+    render json: @place
+  end
 
 end
